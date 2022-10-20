cc7c6247c9b045d9403306eef36bea047ebbfcd2
DeepakSingh260 <alexmha260@gmail.com>
2022-10-20 22:09:25 +0530

    make it

--- first.py
+++ first.py
@@ -23,6 +23,7 @@ chrome_options.add_argument("--headless")
 chrome_options.binary_location =GOOGLE_CHROME_PATH
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+options.add_argument("window-size=1400,800")
 
 app = Flask(__name__)
 
