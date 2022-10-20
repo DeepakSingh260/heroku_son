0277e3f1fcc240fa17e984b35b649955646dd10c
DeepakSingh260 <alexmha260@gmail.com>
2022-10-20 22:20:37 +0530

    make it

--- first.py
+++ first.py
@@ -46,6 +46,7 @@ def mining(Query):
 async def query(string):
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get("https://www.meesho.com/search?q="+str(string))
+    driver.implicitly_wait(20) 
     el = driver.find_elements(By.CLASS_NAME,"sc-dkPtyc")
     link = []
     for i in el:
